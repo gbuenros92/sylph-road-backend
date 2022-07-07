2374e5b7b9b487cb0d986537c40016b04238f818
gustavo <g.buenrostro1992@gmail.com>
2022-07-07 18:09:58 -0400

    Default profile picture in Schema

--- models/Trainer.js
+++ models/Trainer.js
@@ -36,7 +36,10 @@ const trainerSchema = new Schema ({
             ref: 'Pokemon'
         }
     ],
-    picture: String,
+    picture: {
+        type: String,
+        default: "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg"
+    },
     isLeader: {
         type: Boolean,
         default: false,
